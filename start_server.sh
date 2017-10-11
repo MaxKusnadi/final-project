5cba03b0b90ce7f86c5168c5efbc244ea29caffa
Max Kusnadi <max.kusnadi@gmail.com>
2017-10-11 15:48:16 +0800

    basic login

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git pull
-kill $(ps aux | grep 'assignment-3' | awk '{print $2}')
+kill $(ps aux | grep 'final-project' | awk '{print $2}')
 nohup gunicorn --certfile cert.pem --keyfile key.pem -k gevent -b 0.0.0.0:3040 --worker-connections 10 wsgi:app &
