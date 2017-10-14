b494d221d8c65b6ec4d685ee5be5e76246203a6a
Max Kusnadi <max.kusnadi@gmail.com>
2017-10-14 22:01:01 +0800

    basic login

--- app/views/login.py
+++ app/views/login.py
@@ -3,7 +3,7 @@ import json
 
 from flask.views import MethodView
 from flask_login import login_required, logout_user, current_user
-from flask import request, redirect, url_for
+from flask import request, redirect
 
 from app import app
 from app.controller.login import LoginController
@@ -22,6 +22,21 @@ class LoginView(MethodView):
             return json.dumps(result), status
         else:
             return redirect(LOGIN_URL)
+
+
+class LoginStatusView(MethodView):
+
+    def __init__(self):  # pragma: no cover
+        self.control = LoginController()
+
+    def get(self):
+        logging.info("New /login_status request")
+        if current_user.is_authenticated:
+            result, status = self.control.login_user(current_user)
+            return json.dumps(result), status
+        else:
+            logout_user()  # Destroy cookies
+            return "Not logged in"
 
 
 class LogoutView(MethodView):
@@ -49,3 +64,4 @@ class IvleToken(MethodView):
 app.add_url_rule('/login', view_func=LoginView.as_view('login'))
 app.add_url_rule('/logout', view_func=LogoutView.as_view('logout'))
 app.add_url_rule('/ivle_token', view_func=IvleToken.as_view('ivle_token'))
+app.add_url_rule('/login_status', view_func=LoginStatusView.as_view('login_status'))
