1a7f9ee1383e43b5b0bd7dfe604492ab902894e8
Max Kusnadi <max.kusnadi@gmail.com>
2017-10-26 03:32:01 +0800

    fix bug

--- demo.py
+++ demo.py
@@ -5,30 +5,38 @@ from app.models.group import Group, GroupStaff, GroupStudent
 from app import db
 
 # Corrective db please comment later
-courses = Course.query.all()
-for course in courses:
-    if len(course.groups) == 0:
-        course_staff = CourseStaff.query.filter(CourseStaff.course_id == course.id).all()
-        for x in course_staff:
-            x.is_attached_to_group = True
-            db.session.commit()
+# courses = Course.query.all()
+# for course in courses:
+#     if len(course.groups) == 0:
+#         course_staff = CourseStaff.query.filter(CourseStaff.course_id == course.id).all()
+#         for x in course_staff:
+#             x.is_attached_to_group = True
+#             db.session.commit()
 
 # Creating tutorial class
 TIME = 1508929976
 END_TIME = 1511608376
 
 CY = "e0030878"
-CK = "a0130369"
-MAX = "a0107396"
+MAX = "a0130369"
+CK = "a0107396"
 
 dummy_course = Course.query.filter(Course.course_code == "CS3216-DUMMY").first()
-group = Group(dummy_course, "Dummy Tutorial", "1200", "1300", 5, 0, "SOC", "Tutorial")
-db.session.add(group)
-db.session.commit()
+# group = Group(dummy_course, "Dummy Tutorial", "1200", "1300", 5, 0, "SOC", "Tutorial")
+# db.session.add(group)
+# db.session.commit()
+group = Group.query.get(273)
 
 CY = User.query.filter(User.matric == CY).first()
 CK = User.query.filter(User.matric == CK).first()
 MAX = User.query.filter(User.matric == MAX).first()
+
+group_staff = GroupStaff.query.filter(GroupStaff.group_id == group.id).first()
+db.session.delete(group_staff)
+group_students = GroupStudent.query.filter(GroupStudent.group_id == group.id).all()
+for x in group_students:
+    db.session.delete(x)
+db.session.commit()
 
 group_staff = GroupStaff(CK, group)
 group_student = GroupStudent(CY, group)
@@ -38,13 +46,13 @@ db.session.add(group_student)
 db.session.add(group_student_1)
 db.session.commit()
 
-session = Session(group, "10", TIME, END_TIME)
-db.session.add(session)
-session_1 = Session(group, "11", TIME, END_TIME)
-db.session.add(session_1)
-session_2 = Session(group, "12", TIME, END_TIME)
-db.session.add(session_2)
-db.session.commit()
+# session = Session(group, "10", TIME, END_TIME)
+# db.session.add(session)
+# session_1 = Session(group, "11", TIME, END_TIME)
+# db.session.add(session_1)
+# session_2 = Session(group, "12", TIME, END_TIME)
+# db.session.add(session_2)
+# db.session.commit()
 
 
 # TIME = 1508472000
