8bb2c45b3cdaea1637db13639c578ef13117df25
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-07 00:01:07 +0800

    change start server script

--- app/constants/ivle.py
+++ app/constants/ivle.py
@@ -6,13 +6,13 @@ IVLE_URL = "https://ivle.nus.edu.sg/api/Lapi.svc/"
 
 if DEVELOPMENT:
     redirect_url = "http://localhost"
-    LOGIN_REDIRECT_URL_DEV = "http://localhost:3040/login"
+    LOGIN_REDIRECT_URL_DEV = "http://localhost/login"
 else:
     redirect_url = "https://imgratefultoday.com"
     LOGIN_REDIRECT_URL_DEV = "http://localhost:3000"
 LOGIN_REDIRECT_URL_LIVE = "https://nusattend.firebaseapp.com/"
-LOGIN_URL = "https://ivle.nus.edu.sg/api/login/?apikey={api_key}&url={redirect_url}:3040/ivle_token".format(api_key=API_KEY,
-                                                                                                            redirect_url=redirect_url)
+LOGIN_URL = "https://ivle.nus.edu.sg/api/login/?apikey={api_key}&url={redirect_url}/ivle_token".format(api_key=API_KEY,
+                                                                                                       redirect_url=redirect_url)
 
 # LIST OF SERVICE
 VALIDATE_URL = "Validate"
