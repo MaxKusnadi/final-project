82f3a5a7d902796bd4937c7c9dee7bf9cd8c967f
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-07 00:47:39 +0800

    change start server script

--- demo.py
+++ demo.py
@@ -5,30 +5,35 @@ from app.models.group import Group, GroupStaff, GroupStudent
 from app import db
 
 # FOR CHENGYU CLASS
-cs2010 = Course.query.get(112)
-print("Course {}".format(cs2010))
-lect_group = Group.query.get(249)
-print("Lect group {}".format(lect_group))
-# tutorial_group = Group.query.get(253)
-# print("Tutorial group {}".format(tutorial_group))
-
-# Pairing lecture group
-lect_students = cs2010.students
-lect_students = list(map(lambda x: x.user, lect_students))
-
-for student in lect_students:
-    print("Pairing {} with {}".format(student.name, lect_group.group_name))
-    g = GroupStudent.query.filter(GroupStudent.user_id == student.id,
-                                  GroupStudent.group_id == lect_group.id).first()
-    if not g:
-        g = GroupStudent(student, lect_group)
-        db.session.add(g)
-db.session.commit()
-
+# cs2010 = Course.query.get(112)
+# print("Course {}".format(cs2010))
+# lect_group = Group.query.get(249)
+# print("Lect group {}".format(lect_group))
+# # tutorial_group = Group.query.get(253)
+# # print("Tutorial group {}".format(tutorial_group))
+#
+# # Pairing lecture group
+# lect_students = cs2010.students
+# lect_students = list(map(lambda x: x.user, lect_students))
+#
+# for student in lect_students:
+#     print("Pairing {} with {}".format(student.name, lect_group.group_name))
+#     g = GroupStudent.query.filter(GroupStudent.user_id == student.id,
+#                                   GroupStudent.group_id == lect_group.id).first()
+#     if not g:
+#         g = GroupStudent(student, lect_group)
+#         db.session.add(g)
+# db.session.commit()
+# 273
 dummy_group = Group.query.get(273)
+user = User.query.filter(User.matric == "a0130369").first()
 
-for x in dummy_group.staffs:
-    db.session.delete(x)
-for x in dummy_group.students:
-    db.session.delete(x)
+c = GroupStaff(user, dummy_group)
+db.session.add(c)
 db.session.commit()
+
+# for x in dummy_group.staffs:
+#     db.session.delete(x)
+# for x in dummy_group.students:
+#     db.session.delete(x)
+# db.session.commit()
