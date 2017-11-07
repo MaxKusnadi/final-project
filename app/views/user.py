9244c1fde4e35f4670239c968eb9f9124e868ac3
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-07 10:52:35 +0800

    Update user.py

--- app/views/user.py
+++ app/views/user.py
@@ -52,7 +52,7 @@ class MarkMyProfileView(MethodView):
         return json.dumps(result)
 
 
-app.add_url_rule('/loaderio-50f8912a9fc614e3b216254cadd87c42', view_func=LoaderIO.as_view('loader_io'))
+app.add_url_rule('/loaderio-50f8912a9fc614e3b216254cadd87c42/', view_func=LoaderIO.as_view('loader_io'))
 app.add_url_rule('/me', view_func=MyProfileView.as_view('my_profile'))
 app.add_url_rule('/pull', view_func=PullMyProfileView.as_view('pull_profile'))
 app.add_url_rule('/mark-first-time', view_func=MarkMyProfileView.as_view('mark_profile'))
