a61ca4a0231035c166244b8c1dcada748c4bb070
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-11 01:25:33 +0800

    change server

--- start_server.sh
+++ start_server.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 git pull
 kill $(ps aux | grep 'final-project' | awk '{print $2}')
-nohup gunicorn --certfile cert.pem --keyfile key.pem -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 -b 0.0.0.0:8000 wsgi:app &
+nohup gunicorn --certfile cert.pem --keyfile key.pem --worker-class eventlet -w 1 -b 0.0.0.0:8000 wsgi:app &
