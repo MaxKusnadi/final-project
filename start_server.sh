4e4dfa35aacb6a40996639f6dc2ea04d9577fc63
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-14 03:17:55 +0800

    implememnt multiple servers

--- start_server.sh
+++ start_server.sh
@@ -2,3 +2,6 @@
 git pull
 kill $(ps aux | grep 'final-project' | awk '{print $2}')
 nohup gunicorn --certfile cert.pem --keyfile key.pem --worker-class eventlet -w 1 -b 0.0.0.0:8000 wsgi:app &
+nohup gunicorn --certfile cert.pem --keyfile key.pem --worker-class eventlet -w 1 -b 0.0.0.0:8001 wsgi:app &
+nohup gunicorn --certfile cert.pem --keyfile key.pem --worker-class eventlet -w 1 -b 0.0.0.0:8002 wsgi:app &
+nohup gunicorn --certfile cert.pem --keyfile key.pem --worker-class eventlet -w 1 -b 0.0.0.0:8003 wsgi:app &
