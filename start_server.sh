d048cb19f491827d06b65afcf67ccb387b9b8dea
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-15 04:10:50 +0800

    add week_name to session

--- start_server.sh
+++ start_server.sh
@@ -6,3 +6,7 @@ nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8001 wsgi:app --worker-co
 nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8002 wsgi:app --worker-connections 5000 &
 nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8003 wsgi:app --worker-connections 5000 &
 nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8004 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8005 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8006 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8007 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8008 wsgi:app --worker-connections 5000 &
