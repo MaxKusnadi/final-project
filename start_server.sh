543dbdcec1e6503a025ba9dec2eff83d822eaa94
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-14 09:35:06 +0800

    Update start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -5,3 +5,4 @@ nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8000 wsgi:app &
 nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8001 wsgi:app &
 nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8002 wsgi:app &
 nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8003 wsgi:app &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8004 wsgi:app &
