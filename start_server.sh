976e0448c971b71839d67b26a5346197bf11f451
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-15 00:45:06 +0800

    change gunicorn

--- start_server.sh
+++ start_server.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 git pull
 kill $(ps aux | grep 'final-project' | awk '{print $2}')
-nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8000 wsgi:app &
-nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8001 wsgi:app &
-nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8002 wsgi:app &
-nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8003 wsgi:app &
-nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8004 wsgi:app &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8000 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8001 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8002 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8003 wsgi:app --worker-connections 5000 &
+nohup gunicorn --worker-class eventlet -w 1 -b 0.0.0.0:8004 wsgi:app --worker-connections 5000 &
