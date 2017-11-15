b6c81973e8453ea14afdb4c147c60b11424f445c
Max Kusnadi <max.kusnadi@gmail.com>
2017-11-15 13:13:31 +0800

    add week_name to session

--- demo.py
+++ demo.py
@@ -4,48 +4,34 @@ from app.models.course import Course, CourseStaff
 from app.models.group import Group, GroupStaff, GroupStudent
 from app import db
 
-# FOR CHENGYU CLASS
-cs2010 = Course.query.get(104)
-print("Course {}".format(cs2010))
-lect_group = Group.query.get(193)
-print("Lect group {}".format(lect_group))
-# tutorial_group = Group.query.get(253)
-# print("Tutorial group {}".format(tutorial_group))
-
-# Pairing lecture group
-lect_students = cs2010.students
-lect_students = list(map(lambda x: x.user, lect_students))
-
-for student in lect_students:
-    print("Pairing {} with {}".format(student.name, lect_group.group_name))
-    g = GroupStudent.query.filter(GroupStudent.user_id == student.id,
-                                  GroupStudent.group_id == lect_group.id).first()
-    if not g:
-        g = GroupStudent(student, lect_group)
-        db.session.add(g)
+# FOR STEPS
+steps = Course("anand", "Anand Bhojan", "steps", "STEPS2017", "11th Steps 2017", "2017/2018", 1)
+db.session.add(steps)
 db.session.commit()
-# 273
-# dummy_group = Group.query.get(258)
-# # user = User.query.filter(User.matric == "a0130369").first()A0130717
-# user = User.query.filter(User.name == "LAI CHENG YU").first()
-# c = GroupStaff(user, dummy_group)
-# db.session.add(c)
-# db.session.commit()
 
-# for x in dummy_group.staffs:
-#     db.session.delete(x)
-# for x in dummy_group.students:
-#     db.session.delete(x)
-# db.session.commit()
+print(steps)
+print(steps.id)
 
-# dummy_group = Group.query.get(273)
-# user = User.query.filter(User.name == "YUE KAIDI").first()
-# c = GroupStudent(user, dummy_group)
-# db.session.add(c)
-# db.session.commit()
+sj = User.query.filter(User.name == "LAU SHI JIE").first()
+cy = User.query.filter(User.name == "LAI CHENG YU").first()
+ck = User.query.filter(User.name == "CHAN KHAN").first()
+mk = User.query.filter(User.matric == "a0130369").first()
 
-# s = Session(dummy_group, "13", 1510339446, 1510944246)
-# db.session.add(s)
-# db.session.commit()
+staffs = [sj, cy, ck, mk]
 
+for x in staffs:
+    print("Pairing {} with {}".format(x, steps))
+    cs = CourseStaff(x, steps)
+    db.session.add(cs)
+db.session.commit()
 
+g = Group(steps, "11th STEPS 2017", "1800", "2200", 3, 0, "SOC", "LECTURE")
+db.session.add(g)
+db.session.commit()
+
+print(g)
+print(g.id)
+
+s = Session(g, "13", 1510740000, 1510754400)
+db.session.add(s)
+db.session.commit()
